a4229baf0d1f554e4277b244f2376c158c15652b
Miroslav Lacina <miroslav.lacina@gmail.com>
2016-05-20 14:17:51 +0200

    Fix pattern for nested paths

--- server/src/main/java/com/github/iviireczech/springreact/controller/SinglePageApplicationController.java
+++ server/src/main/java/com/github/iviireczech/springreact/controller/SinglePageApplicationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SinglePageApplicationController {
 
     // Match everything without a suffix (so not a static resource)
-    @RequestMapping(value = "/{path:[^\\.]*}")
+    @RequestMapping(value = "/**/{path:[^\\.]*}")
     public String redirect() {
         // Forward to home page so that route is preserved.
         return "forward:/";
