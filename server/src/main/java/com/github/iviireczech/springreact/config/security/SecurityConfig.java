440484722cb8ac7d24fb30fa8debb5d63905ba24
Miroslav Lacina <miroslav.lacina@gmail.com>
2016-05-25 13:05:56 +0200

    Add basic security configuration

---
+++ server/src/main/java/com/github/iviireczech/springreact/config/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.github.iviireczech.springreact.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ADMIN")
+                .and()
+                .withUser("user").password("user").roles("USER");
+    }
+
+}
