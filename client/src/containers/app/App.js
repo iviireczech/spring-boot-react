ca0550cf119f0b5b4be2fbc42e90b592678378b7
Miroslav Lacina <miroslav.lacina@gmail.com>
2016-05-26 17:28:34 +0200

    Change App layout, introduce Navbar component

--- client/src/containers/app/App.js
+++ client/src/containers/app/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {Grid, Col, Row} from 'react-bootstrap';
+
+import Navbar from '../../components/navbar/Navbar';
+
+import {logoutUser} from './../../actions/logout/logout';
 
 import './App.scss'
 
-export default class App extends React.Component {
+class App extends React.Component {
     render() {
+        
         return (
             <div>
-                {this.props.children}
+                <Navbar
+                    isAuthenticated={this.props.isAuthenticated}
+                    logout={this.props.logout}
+                />
+                <Grid>
+                    <Row>
+                        <Col md={12}>
+                            {this.props.children}
+                        </Col>
+                    </Row>
+                </Grid>
             </div>
         );
     }
@@ -15,3 +32,21 @@ export default class App extends React.Component {
 App.propTypes = {
     children: React.PropTypes.node
 };
+
+function mapStateToProps(state) {
+
+    return {
+        isAuthenticated: state.authentication.isAuthenticated
+    }
+    
+}
+
+function mapDispatchToProps(dispatch) {
+    
+    return {
+        logout: () => dispatch(logoutUser())
+    }
+    
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
