59da1b7f62d7b844353a3bc353143afd73ff5488
Miroslav Lacina <miroslav.lacina@gmail.com>
2016-05-26 15:15:35 +0200

    Add authentication reducer

---
+++ client/src/reducers/authentication/authentication.js
@@ -0,0 +1,39 @@
+import { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE } from '../../actions/login/login';
+import { LOGOUT_REQUEST, LOGOUT_SUCCESS } from '../../actions/logout/logout';
+
+export default function authentication(state = {
+    isFetching: false,
+    isAuthenticated: localStorage.getItem('access_token') ? true : false
+}, action) {
+    switch (action.type) {
+        case LOGIN_REQUEST:
+            return Object.assign({}, state, {
+                isFetching: true,
+                isAuthenticated: false,
+                credentials: action.payload.credentials
+            });
+        case LOGIN_SUCCESS:
+            return Object.assign({}, state, {
+                isFetching: false,
+                isAuthenticated: true
+            });
+        case LOGIN_FAILURE:
+            return Object.assign({}, state, {
+                isFetching: false,
+                isAuthenticated: false,
+                error: action.payload.error
+            });
+        case LOGOUT_REQUEST:
+            return Object.assign({}, state, {
+                isFetching: true,
+                isAuthenticated: false
+            });
+        case LOGOUT_SUCCESS:
+            return Object.assign({}, state, {
+                isFetching: true,
+                isAuthenticated: false
+            });
+        default:
+            return state
+    }
+}
--- client/src/reducers/reducers.js
+++ client/src/reducers/reducers.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
+import authentication from './authentication/authentication'
+
 export default combineReducers({
+    authentication: authentication,
     routing: routerReducer
 });
