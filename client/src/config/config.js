03b68a2d8eee1a42186830ffcd48e790f38b3adb
Miroslav Lacina <miroslav.lacina@gmail.com>
2016-05-26 15:33:03 +0200

    Add clientId and clientSecret

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,4 +1,6 @@
 module.exports = {
+    clientId: 'web',
+    clientSecret: '6b7b5d6028865dc2901f0b0db64cb291',
     apiHost: process.env.API_HOST || 'localhost',
     apiPort: process.env.API_PORT,
     apiUrl: 'http://' + (process.env.API_HOST || 'localhost') + (process.env.API_PORT ? ':' + process.env.API_PORT : '')
