f79e8bb412c093cb0eee435c310bcc512bad4934
Miroslav Lacina <miroslav.lacina@gmail.com>
2016-05-26 17:23:56 +0200

    Change homepage layout

--- client/src/containers/home/Home.js
+++ client/src/containers/home/Home.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import {Grid, Col, Row, PageHeader} from 'react-bootstrap';
+import { PageHeader } from 'react-bootstrap';
 
 export default class Home extends React.Component {
     render() {
         return (
-            <Grid>
-                <Row>
-                    <Col md={12}>
-                        <PageHeader>It works!</PageHeader>
-                    </Col>
-                </Row>
-            </Grid>
+            <PageHeader>Spring & React Starter Kit</PageHeader>
         );
     }
 }
