c0b06cfb938c192fe49b9ab26d630a940bac7d0a
Miroslav Lacina <miroslav.lacina@gmail.com>
2016-06-07 10:16:36 +0200

    Remove unused 'isFetching' property

--- client/src/reducers/authentication/authentication.js
+++ client/src/reducers/authentication/authentication.js
@@ -2,34 +2,28 @@ import { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE } from '../../actions/login
 import { LOGOUT_REQUEST, LOGOUT_SUCCESS } from '../../actions/logout/logout';
 
 export default function authentication(state = {
-    isFetching: false,
     isAuthenticated: localStorage.getItem('access_token') ? true : false
 }, action) {
     switch (action.type) {
         case LOGIN_REQUEST:
             return Object.assign({}, state, {
-                isFetching: true,
                 isAuthenticated: false
             });
         case LOGIN_SUCCESS:
             return Object.assign({}, state, {
-                isFetching: false,
                 isAuthenticated: true
             });
         case LOGIN_FAILURE:
             return Object.assign({}, state, {
-                isFetching: false,
                 isAuthenticated: false,
                 errorMessage: action.payload.errorMessage
             });
         case LOGOUT_REQUEST:
             return Object.assign({}, state, {
-                isFetching: true,
                 isAuthenticated: false
             });
         case LOGOUT_SUCCESS:
             return Object.assign({}, state, {
-                isFetching: true,
                 isAuthenticated: false
             });
         default:
